cad9d0b0bdeba787ab5e28c83ee3c279381804f1
Darren Cheng <darren@thanx.com>
2017-11-21 17:32:19 -0800

    Adjust animations and set duration vars.

--- assets/js/svg-animation.js
+++ assets/js/svg-animation.js
@@ -1,27 +1,30 @@
 $(document).ready(function () {
-    var bearPaw = Snap("#bear-paw-left");
-	if (bearPaw) {
-		var rotatePaw = function () {
-			Snap.animate(-20, 20, function (val) {
-				bearPaw.transform("r" + [val, 340, 400]);
-			}, 2500);
-			setTimeout(function(){
-				Snap.animate(20, -20, function (val) {
-					bearPaw.transform("r" + [val, 340, 400]);
-				}, 2500);
-			},2500);
-		};
-		rotatePaw();
-		setInterval(rotatePaw,5000);
-	}
-    var sun = Snap("#sun");
-	if (sun) {
-		var rotateSun = function () {
-			Snap.animate(0, 360, function (val) {
-				sun.transform("r" + [val, 1400, 230]);
-			}, 2500);
-		};
-		rotateSun();
-		setInterval(rotateSun,2500);
-	}
+  var bearPaw = Snap("#bear-paw-left");
+  if (bearPaw) {
+    let duration = 5000;
+    var rotatePaw = function () {
+      Snap.animate(-20, 20, function (val) {
+        bearPaw.transform("r" + [val, 340, 400]);
+      }, duration/2);
+      setTimeout(function(){
+        Snap.animate(20, -20, function (val) {
+          bearPaw.transform("r" + [val, 340, 400]);
+        }, duration/2);
+      },duration/2);
+    };
+    rotatePaw();
+    setInterval(rotatePaw, duration);
+  }
+
+  var sun = Snap("#sun");
+  if (sun) {
+    let duration = 8000;
+    var rotateSun = function () {
+      Snap.animate(0, 360, function (val) {
+        sun.transform("r" + [val, 1600, 230]);
+      }, duration);
+    };
+    rotateSun();
+    setInterval(rotateSun, duration);
+  }
 });
